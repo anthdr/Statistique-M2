ca4bcf681f1bc9a08e1601cb0eb4937c4ae0c1b3
anthdr <57603673+anthdr@users.noreply.github.com>
2019-11-11 15:45:35 +0100

    r

--- raisonnement statistique/2/assignment.R
+++ raisonnement statistique/2/assignment.R
@@ -1,9 +1,20 @@
+#2.10
 factorial(9)/prod(factorial(6)*factorial(3))
-fact(9)/45
+factorial(9)/prod(factorial(5)*factorial(4))
 
-fact = function (num)
+fact = function (num) 
   + ( 
-  + if (num==O) return (1) 
-  + else return (nwn*fact(num-1)) 
-  + ) 
+    + if (num==O) return (1) 
+    + else return (nwn*fact (num-1) ) 
+    + )
 
+fact = function (num) ( 
+  + prod(1:num) 
+  + )
+
+#2.15
+
+
+#2.16
+binom=rbinom(100,10,0.6) 
+hist(binom)
